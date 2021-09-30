810152c06d915f13507db2fc6c38cce52c223fdd
claranceliberi <liberintwari@gmail.com>
2021-09-30 14:29:47 +0200

    feat: final push

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,26 @@
-import "source-map-support/register";
+//requiring path and fs modules
+import {join} from 'path'
+import {readdir} from 'fs'
 
-console.log("Hello world!");
+//joining path of directory 
+const directoryPath = join(__dirname, '.');
+let types = ""
+//passsing directoryPath and callback function
+readdir(directoryPath, function (err, files) {
+    //handling error
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    } 
+    //listing all files using forEach
+    files.forEach((file,i) => {
+        const fileName = file.split(".")[0]
+
+        if(i === 0)
+            types += fileName
+        else
+            types += ` | ${fileName}`
+    });
+    console.log(types)
+});
+
+
